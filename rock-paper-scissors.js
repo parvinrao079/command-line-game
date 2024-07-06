3578852dbe50497e6a327bc57ab6d35aeb19fe40
parvinrao079 <parvinrao079@gmail.com>
2024-07-06 15:31:20 +0200

    rock,paper,scissors game  done and can be played by triggering the node.js directly

---
+++ rock-paper-scissors.js
@@ -0,0 +1,40 @@
+//Importing the "process" library in order to access the arguments of command lines
+const processLib = require('process');
+
+//Gets the players move from the command line
+const playerMoves = process.argv[2];
+
+//Possible moves
+const moves = ["rock", "paper", "scissors"];
+
+//this function is to generate random moves
+function randomMoves()
+{
+    const randomSelect = Math.floor(Math.random() * moves.length);
+    return moves[randomSelect];
+}
+
+//gets computer move
+const computerMoves = randomMoves();
+
+//this function is to determine the winner
+function Winner(player,computer)
+{
+    if(player == computer)
+    {
+        return console.log("The game is draw!");
+    }
+    
+    if((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper"))
+    {
+        return console.log("You Win!");
+    }
+    return console.log("You Lost!");
+}
+
+//logging the moves output
+console.log(`Player move: ${playerMoves}`);
+console.log(`Computer move: ${computerMoves}`);
+
+//Dtermining who is the winner
+Winner(playerMoves, computerMoves);
